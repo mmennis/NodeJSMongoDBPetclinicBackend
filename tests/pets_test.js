1483678f69c62076a78bca9171de1d7d2c48aa63
Michael Mennis <mmennis@gmail.com>
2019-09-13 15:14:54 -0700

    minor changes

--- tests/pets_test.js
+++ tests/pets_test.js
@@ -57,41 +57,71 @@ describe('Pets model', () => {
                 done();
             })
             .catch((err) => {
-                console.error(`Problem saving the pet ${err}`);
+                console.error(`CREATE no visits - ${err}`);
                 done();
             });
     });
 
-    it('should save a pet with owner and visits', (done) => {
+    it('should save a pet with an owner but empty visits ', (done) => {
         const pet = new Pet({
-            name: 'fido with visits',
+            name: 'fido no visits',
             owner: owner,
             pet_type: 'dog',
-            visits: [
-                {
-                    visit_date: Date.now(),
-                    reason: 'Sore paw',
-                    vet: null,
-                }
-            ]
+            visits: []
         });
         pet.save()
             .then(() => {
+                //console.log('Saved pet');
                 assert(!pet.isNew);
-                Pet.findById(pet._id)
-                    .then((savedPet) => {
-                        assert((pet._id).equals(savedPet._id));
-                        done();
-                    })
-                    .catch((err) => {
-                        console.log(`CREATE with visits - problem ${err}`);
-                        done();
-                    });
+                assert(pet.visits.length === 0);
+                done();
             })
             .catch((err) => {
-                console.error(`Problem saving the pet ${err}`);
+                console.error(`CREATE empty visits - ${err}`);
                 done();
             });
+    });
+
+    
+
+    describe('with pets and visits', () => {
+        let pet;
+        beforeEach((done) => {
+            pet = new Pet({
+                name: 'fido with visits',
+                owner: owner,
+                pet_type: 'dog',
+                visits: [
+                    {
+                        visit_date: Date.now(),
+                        reason: 'Sore paw',
+                        vet: null,
+                    }
+                ]
+            });
+            pet.save()
+                .then(() => {
+                    assert(!pet.isNew);
+                    done();    
+                })
+                .catch((err) => {
+                    console.error(`CREATE pet - ${err}`);
+                    done();
+                });
+        })
+
+        it('should save a pet with owner and visits', (done) => {
+            Pet.findById(pet._id)
+                .then((savedPet) => {
+                    assert((pet._id).equals(savedPet._id));
+                    assert(savedPet.visits.length > 0);
+                    done();
+                })
+                .catch((err) => {
+                    console.log(`CREATE with visits - ${err}`);
+                    done();
+                });
+        });    
     });
 
 });
